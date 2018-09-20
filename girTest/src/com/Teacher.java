4b97964ccdacf645d27b82105958da4ab1222aca
mengyy    mengyy@asiainfo.com <yongyongmeng1108@163.com>
2018-09-20 15:18:29 +0800

    代码提交

---
+++ girTest/src/com/Teacher.java
@@ -0,0 +1,15 @@
+package com;
+
+public class Teacher {
+
+	public void zoo() {
+		Personal personal= new Personal();
+		personal.setAddress("jiaxing");
+		personal.setAge("18");
+		personal.setFlag("1");
+		personal.setName("张三");
+		personal.setPhone("13567899001");
+		System.out.println("出参"+personal.toString());
+	}
+	
+}

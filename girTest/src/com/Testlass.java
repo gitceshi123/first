5da02d74cc5e8060e4424051e23ab5f76f03da36
mengyy    mengyy@asiainfo.com <yongyongmeng1108@163.com>
2018-09-20 13:08:57 +0800

    girTest/src/com/Testlass.java
    新增

---
+++ girTest/src/com/Testlass.java
@@ -0,0 +1,11 @@
+package com;
+
+public class Testlass {
+	
+	public static void main(String[] args) {
+		TestGit  testGit = new TestGit();
+		int sum = testGit.sum(1,2);
+		System.out.println("sum"+sum);
+	}
+
+}
